e22ca04df2cc434fd1c66470114d1a96566e3837
abissier <71886314+abissier@users.noreply.github.com>
2021-05-25 09:35:43 -0700

    day 1 challenge

---
+++ src/DisplayData.js
@@ -0,0 +1,57 @@
+import ScotchInfoBar from './ScotchInfoBar';
+
+/**
+ * Our data
+ * ------------------------
+ */
+const user = {
+	name: 'Chris on Code',
+	location: 'Las Vegas',
+	foodType: 'Everything',
+	age: 28,
+	likes: 'Coding into the wee hours of the morning',
+	twitterUsername: 'chrisoncode',
+	avatar: 'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+};
+
+/**
+ * Our React component where we should display data
+ * ------------------------
+ */
+
+function DisplayData() {
+	return (
+		<div className="App">
+			<div className="user-deets">
+				<img alt={user.name} src={user.avatar} />
+				<a href={`https://twitter.com/${user.twitterUsername}`}>
+					<h3>{user.name}</h3>
+				</a>
+				<p>
+					<strong>Location</strong>
+					{user.location}
+				</p>
+				<p>
+					<strong>Eats</strong>
+					{user.foodType}
+				</p>
+				<p>
+					<strong>Age</strong>
+					{user.age}
+				</p>
+				<p>
+					<strong>Likes</strong>
+
+					{user.likes}
+				</p>
+				<p>
+					<strong>Twitter</strong>
+					<a href={`https://twitter.com/${user.twitterUsername}`}>@{user.twitterUsername}</a>
+				</p>
+			</div>
+			<ScotchInfoBar />
+		</div>
+	);
+}
+
+export default DisplayData;
