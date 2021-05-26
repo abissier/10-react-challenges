76836efacf722fb9bf8fc4a2653cebd46933dc65
abissier <71886314+abissier@users.noreply.github.com>
2021-05-26 10:35:15 -0700

    day 3 usestate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import DisplayData from './Day1/DisplayData';
-import LoopOverArray from './Day2/LoopOverArray';
+// import LoopOverArray from './Day2/LoopOverArray';
+import UpdateDOM from './Day3/UpdateDOM';
 
 function App() {
 	return (
 		<div>
 			{/* <DisplayData />; */}
-			<LoopOverArray />
+			{/* <LoopOverArray /> */}
+			<UpdateDOM />
 		</div>
 	);
 }
